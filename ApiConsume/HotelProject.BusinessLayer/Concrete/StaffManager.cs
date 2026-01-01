8929081345931a53513052706623d318e25ec3c5
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2026-01-01 16:19:29 +0300

    Implement layered architecture and initial EF migration
    Added project references between layers and removed placeholder files. Introduced entity models, service and DAL interfaces, and manager classes for CRUD operations. Implemented generic repository pattern and EF Core context. Added initial migration files for database schema. Included adımlar.txt for API testing notes.

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/StaffManager.cs
@@ -0,0 +1,39 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class StaffManager(IStaffDal _staffDal) : IStaffService
+    {
+        public void TDelete(Staff t)
+        {
+            _staffDal.Delete(t);
+        }
+
+        public Staff TGetByID(int id)
+        {
+           return _staffDal.GetByID(t);
+        }
+
+        public List<Staff> TGetList()
+        {
+            return _staffDal.GetList(t);
+        }
+
+        public void TInsert(Staff t)
+        {
+            _staffDal.Insert(t);
+        }
+
+        public void TUpdate(Staff t)
+        {
+            _staffDal.Update(t);
+        }
+    }
+}
--- ApiConsume/HotelProject.EntityLayer/Concrete/Service.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Service.cs
@@ -1,6 +1,6 @@
 ﻿namespace HotelProject.EntityLayer.Concrete
 {
-    public class Services
+    public class Service
     {
         public int ServiceID { get; set; }
         public string ServiceIcon { get; set; }
